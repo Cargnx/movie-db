39971b9c94cf0c8322c1a5c2b807df95b99a0043
Cargnx <117575681+Cargnx@users.noreply.github.com>
2025-08-16 02:46:41 +0200

    [WIP] Adjust footer content

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,13 +3,11 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-gray-900/20 backdrop-blur-sm border-t border-gray-700/50 text-white shadow-[0px_27px_5px_0px_rgba(0,_0,_0,_0.1)]">
-      <div className="flex justify-between items-center  container-xl py-7">
-        <p className="font-bold">HELLO WORLD</p>
-        <ul className="flex space-x-6">
-          <li className="hover:text-gray-300 cursor-pointer">TEST</li>
-          <li className="hover:text-gray-300 cursor-pointer">TEST</li>
-          <li className="hover:text-gray-300 cursor-pointer">TEST</li>
-        </ul>
+      <div className="flex items-center  justify-center container-xl py-7 gap-6">
+        <h1 className="text-md capitalize font-bold">Movie DB</h1>
+        <a href="https://github.com/Cargnx" className="font-bold text-xs">
+          My GitHub
+        </a>
       </div>
     </footer>
   );
