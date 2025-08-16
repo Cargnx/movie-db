2feb7d9791794e99e1d8a6de886298462eec79da
Cargnx <117575681+Cargnx@users.noreply.github.com>
2025-08-16 02:27:06 +0200

    [WIP] Create card component with custom titles and images, test cards on page.jsx

---
+++ src/app/components/Cards.jsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+export default function Cards({
+  hasImage = false,
+  image,
+  alt,
+  title,
+  description,
+}) {
+  return (
+    <a
+      href="https://www.youtube.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="border border-accent-dark-gray rounded-lg shadow-lg bg-black w-[390px]"
+    >
+      {hasImage && (
+        <div className="relative w-full h-[250px] flex items-center justify-center">
+          {image ? (
+            <Image src={image} alt={alt} fill style={{ objectFit: "cover" }} />
+          ) : (
+            <Image
+              src="/image-placeholder.svg"
+              alt="Image Placeholder"
+              width={100}
+              height={200}
+              style={{ objectFit: "cover" }}
+            />
+          )}
+        </div>
+      )}
+
+      <div className="p-5 max-h-[150px] gap-4 flex flex-col">
+        <span className="card-title block text-white line-clamp-2">
+          {title}
+        </span>
+        <p className="card-description text-gray-400 line-clamp-3">
+          {description}
+        </p>
+      </div>
+    </a>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,52 @@
+import Cards from "./components/Cards";
+
 export default function Home() {
   return (
-    <div className="col-span-full text-center pt-40">
-      <h1 className="text-7xl capitalize font-bold mb-4">hello world</h1>
-      <a href="https://github.com/Cargnx" className="font-bold">
-        My GitHub
-      </a>
+    <div className="pt-40">
+      <div className="text-center">
+        <h1 className="text-7xl capitalize font-bold mb-4">Movie DB</h1>
+        <a href="https://github.com/Cargnx" className="font-bold">
+          My GitHub
+        </a>
+      </div>
+
+      <div className="pt-20 grid grid-cols-3 gap-y-7">
+        <Cards
+          hasImage={true}
+          image="/sw-episode-3.jpg"
+          alt="Image description"
+          title="Card Title"
+          description="HELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLDHELLO WORLD"
+        />
+
+        <Cards
+          hasImage={true}
+          image=""
+          alt="Image description"
+          title="Card Title"
+          description="HELLO WORLD"
+        />
+
+        <Cards
+          hasImage={true}
+          image="/Django.jpg"
+          alt="Image description"
+          title="Card Title"
+          description="HELLO WORLD"
+        />
+
+        <Cards
+          alt="Image description"
+          title="WHATS UP"
+          description="HELLO WORLD"
+        />
+
+        <Cards
+          alt="Image description"
+          title="WHATS UP"
+          description="HELLO WORLD"
+        />
+      </div>
     </div>
   );
 }
